1d5eea53f1ab542c1ea5a3d9e88843c9f67a146a
recargasbar@gmail.com <926041@senacminas.edu.br>
2024-08-14 14:06:08 -0300

    feat: Sortea nums s/ repetir

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import random
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class BingoApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Bingo')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        #Lista de números sorteados
+        self.ls_num_sorteados = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        #Label para exibir números sorteados
+        self.lbl_result = tk.Label(root, text='', font=('Arial', 14))
+        self.lbl_result.pack(pady=20)
+
+
+        #Botão para sortear números
+        self.btn_sortear = tk.Button(root, text="Sortear", command=self.sortear_numeros, font=('Arial', 14))
+        self.btn_sortear.pack(pady=10)
+
+        #Botão para reiniciar o jogo
+        self.btn_reset = tk.Button(root, text="Reiniciar o jogo", command=self. reset_jogo, font=('Arial', 14))
+        self.btn_reset.pack(pady=10)
+
+        #Função de sortear um número de 1 a 75 e adiciona a lista
+    def sortear_numeros(self):
+        if len(self.ls_num_sorteados) < 75:
+            numeroSorteado = random.randint(1, 75)
+            while numeroSorteado in self.ls_num_sorteados:
+                numeroSorteado = random.randint(1, 75)
+            self.ls_num_sorteados.append(numeroSorteado)
+            self.lbl_result.config(text=', '.join(map(str, self.ls_num_sorteados)))
+        else:
+            self.lbl_result.config(text="Bingo encerrado!")
+
+
+    def reset_jogo(self):
+        self.ls_num_sorteados.clear()
+        self.lbl_result.config(text='')
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = BingoApp(root)
+    root.mainloop()
